0b8e425c373c203355c5011f4f052a3dcc57d6c1
Tim Hagemann <tim@infinityblade.de>
2014-11-16 17:26:43 +0100

    Added measurements for insertion

--- aufgabe_2/src/insertions.erl
+++ aufgabe_2/src/insertions.erl
@@ -13,39 +13,45 @@
 -export([insertionS/3]).
 
 insertionS(ARRAY, START, END) ->
-  insertionS(ARRAY, START, END, 1).
+  insertionS({ ARRAY, 0, 0 }, START, END, 1).
 
-insertionS(ARRAY, START, END, CURRENT) when CURRENT > END ->
-  ARRAY;
+insertionS({ ARRAY, SWAPS, COMPARISON }, _START, END, CURRENT) when CURRENT > END ->
+  { ARRAY, { SWAPS, COMPARISON }};
 
-insertionS(ARRAY, START, END, CURRENT) ->
-  io:write(END),
+insertionS({ ARRAY, SWAPS, COMPARISON }, START, END, CURRENT) ->
   CURR_ELEM = array:getA(ARRAY, CURRENT),
-  INSERT_POS = findInsert(ARRAY, START, CURRENT - 1, CURR_ELEM),
-  TEMP = insertAt(ARRAY, INSERT_POS, CURRENT),
-  io:write(TEMP),
-  insertionS(TEMP, START, END, CURRENT + 1).
+  { INSERT_POS, COMPARES } = findInsert(ARRAY, START, CURRENT - 1, CURR_ELEM),
+  { TEMP, SWAP } = insertAt(ARRAY, INSERT_POS, CURRENT),
+  insertionS({ TEMP, SWAPS + SWAP, COMPARES + COMPARISON }, START, END, CURRENT + 1).
 
 findInsert(ARRAY, START, END, ELEM) when START > END ->
-  START;
+  COMPARES = util:countread(compare),
+  util:countreset(compare),
+  { START, COMPARES };
 
 findInsert(ARRAY, START, END, ELEM) ->
   LAST = array:getA(ARRAY, END),
   PREVIOUS = array:getA(ARRAY, END + 1),
   SMALLER = (ELEM > LAST),
   LARGER = (ELEM < PREVIOUS),
+  util:counting(compare, 2),
   CORRECT = SMALLER and LARGER,
   if CORRECT ->
-      END + 1;
+      COMPARES = util:countread(compare),
+      util:countreset(compare),
+      { END + 1, COMPARES };
     true ->
       findInsert(ARRAY, START, END - 1, ELEM)
   end.
 
 insertAt(ARRAY, POS, CURRENT_POS) when POS == CURRENT_POS ->
-  ARRAY;
+  SWAPS = util:countread(swap),
+  util:countreset(swap),
+  { ARRAY, SWAPS};
 
 insertAt(ARRAY, POS, CURRENT_POS) ->
   TEMP_ARRAY = switch(ARRAY, CURRENT_POS - 1, CURRENT_POS),
+  util:counting1(swap),
   insertAt(TEMP_ARRAY, POS, CURRENT_POS - 1).
 
 switch(ARRAY, POS, POS2) ->
