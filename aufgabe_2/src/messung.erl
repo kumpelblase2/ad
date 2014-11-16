9d4ba250b6ef5aada0e4a19abc749f9a9eca0de1
kumpelblase2 <tim@infinityblade.de>
2014-11-16 12:10:53 +0100

    Initial implementation for selection sort and measurments

--- aufgabe_2/src/messung.erl
+++ aufgabe_2/src/messung.erl
@@ -10,7 +10,51 @@
 -author("tim_hagemann").
 
 %% API
--export([startMessung/1]).
+-export([run/0, startMessung/1]).
+
+run() ->
+  startMessung(all).
+
 % Type = { all, insertion, selection }
-startMessung(TYPE) ->
+startMessung(all) ->
+  SIZE = 100,
+  RUNS = 100,
+  { INS_SIZE, INS_ALGORITHM, INS_AVERAGE_TIME, INS_AVERAGE_SWAPS, INS_AVERAGE_COMPARISON  } = doMessung(insertion, RUNS, SIZE),
+  { SEL_SIZE, SEL_ALGORITHM, SEL_AVERAGE_TIME, SEL_AVERAGE_SWAPS, SEL_AVERAGE_COMPARISON  } = doMessung(selection, RUNS, SIZE),
+  INS_DATA = { INS_SIZE, INS_ALGORITHM, INS_AVERAGE_TIME, INS_AVERAGE_SWAPS, INS_AVERAGE_COMPARISON  },
+  SEL_DATA = { SEL_SIZE, SEL_ALGORITHM, SEL_AVERAGE_TIME, SEL_AVERAGE_SWAPS, SEL_AVERAGE_COMPARISON  },
+  file:write_file("messung.dat", io_lib:format("~p\n~p",[INS_DATA, SEL_DATA])),
+  ok;
+
+startMessung(insertion) ->
+  SIZE = 100,
+  RUNS = 100,
+  { INS_SIZE, INS_ALGORITHM, INS_AVERAGE_TIME, INS_AVERAGE_SWAPS, INS_AVERAGE_COMPARISON  } = doMessung(insertion, RUNS, SIZE),
+  INS_DATA = { INS_SIZE, INS_ALGORITHM, INS_AVERAGE_TIME, INS_AVERAGE_SWAPS, INS_AVERAGE_COMPARISON  },
+  file:write_file("messung.dat", io_lib:format("~p",[INS_DATA])),
+  ok;
+
+startMessung(selection) ->
+  SIZE = 100,
+  RUNS = 100,
+  { SEL_SIZE, SEL_ALGORITHM, SEL_AVERAGE_TIME, SEL_AVERAGE_SWAPS, SEL_AVERAGE_COMPARISON  } = doMessung(selection, RUNS, SIZE),
+  SEL_DATA = { SEL_SIZE, SEL_ALGORITHM, SEL_AVERAGE_TIME, SEL_AVERAGE_SWAPS, SEL_AVERAGE_COMPARISON  },
+  file:write_file("messung.dat", io_lib:format("~p",[SEL_DATA])),
+  ok.
+
+
+doMessung(TYPE, RUNS, SIZE) ->
+  RESULT = doMessung(TYPE, RUNS, SIZE, []),
+  ok.
+
+doMessung(TYPE, RUNS, SIZE, TEMP) ->
+  if RUNS > 20 ->
+      ok;
+    RUNS > 10 ->
+      ok;
+    RUNS > 0 ->
+      ok;
+    true ->
+      TEMP
+  end,
   ok.
--- aufgabe_2/src/selections.erl
+++ aufgabe_2/src/selections.erl
@@ -11,5 +11,29 @@
 
 %% API
 -export([selectionS/3]).
+selectionS(ARRAY, START, END) when START > END ->
+  ARRAY;
+
 selectionS(ARRAY, START, END) ->
-  ok.
+  SMALLEST_INDEX = findSmallest(ARRAY, START, END),
+  selectionS(switch(ARRAY, START, SMALLEST_INDEX), START + 1, END).
+
+findSmallest(ARRAY, START, END) ->
+  findSmallest(ARRAY, START, END, 0).
+
+findSmallest(_, START, END, CURRENT) when START > END ->
+  CURRENT;
+
+findSmallest(ARRAY, START, END, CURRENT) ->
+  NEXT = array:getA(ARRAY, START),
+  if NEXT > CURRENT ->
+    findSmallest(ARRAY, START + 1, END, START);
+    true ->
+      findSmallest(ARRAY, START + 1, END, CURRENT)
+  end.
+
+switch(ARRAY, FIRST, SECOND) ->
+  TEMP = array:getA(ARRAY, FIRST),
+  FIRST_SWAP = array:setA(ARRAY, FIRST, array:getA(ARRAY, SECOND)),
+  RESULT = array:setA(FIRST_SWAP, SECOND, TEMP),
+  RESULT.
