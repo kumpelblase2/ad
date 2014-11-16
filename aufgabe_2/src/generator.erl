a2fafe80db72302c0488d0471eed3820b78ca188
Tim Hagemann <tim@infinityblade.de>
2014-11-16 13:36:58 +0100

    Revert fac324b..c3c4bd6
    This rolls back to commit fac324bb7e24b12c445a85ac3c61520f56248505.

--- aufgabe_2/src/generator.erl
+++ aufgabe_2/src/generator.erl
@@ -1,44 +1,16 @@
 %%%-------------------------------------------------------------------
-%%% @author tim_hartig
-%%% @copyright (C) 2014, HAW
+%%% @author tim_hagemann
+%%% @copyright (C) 2014, <COMPANY>
 %%% @doc
 %%%
 %%% @end
 %%% Created : 12. Nov 2014 10:53
 %%%-------------------------------------------------------------------
 -module(generator).
--author("tim_hartig").
+-author("tim_hagemann").
 
 %% API
 -export([sortNum/2]).
 
-
-sortNum(LENGTH, MODE) when LENGTH > 0 ->
-  sortNum(LENGTH, MODE, LENGTH, liste:create()).
-
-% Abbruchbedingung
-sortNum(_, _, 0, ACCU) ->
-  ACCU.
-
-sortNum(LENGTH, random, REMAINING, ACCU) ->
-  sortNum(LENGTH, random, REMAINING - 1, liste:insert(ACCU, liste:laenge(ACCU), random:uniform(LENGTH))).
-
-sortNum(LENGTH, ascending, REMAINING, ACCU) ->
-  sortNum(LENGTH, random, REMAINING - 1, liste:insert(ACCU, liste:laenge(ACCU), LENGTH - REMAINING)).
-
-sortNum(LENGTH, descending, REMAINING, ACCU) ->
-  sortNum(LENGTH, random, REMAINING - 1, liste:insert(ACCU, liste:laenge(ACCU), REMAINING)).
-
-% Ein Versuch, die Methode, welche Art von Nummer generiert wird, zu refaktorisieren, abhängig von MODE --> getNum()
-% Bis auf die Zahl, die als nächstes eingefügen werden soll, ist der Code der gleiche.
-%sortNum(LENGTH, MODE, REMAINING, ACCU, NUMBER) ->
-%  sortNum(LENGTH, MODE, REMAINING - 1, liste:insert(ACCU, liste:laenge(ACCU), NUMBER), getNum(MODE, LENGTH, REMAINING)).
-%
-%genNum(random, LENGTH, _) ->
-%  randon:uniform(LENGTH).
-%
-%genNum(ascending, LENGTH, REMAINING) ->
-%  LENGTH - REMAINING.
-%
-%genNum(descending, LENGTH, REMAINING) ->
-%  REMAINING.
+sortNum(LENGTH, MODE) ->
+  ok.
--- aufgabe_2/src/selections.erl
+++ aufgabe_2/src/selections.erl
@@ -1,36 +1,15 @@
 %%%-------------------------------------------------------------------
-%%% @author tim_hartig
-%%% @copyright (C) 2014, HAW
+%%% @author tim_hagemann
+%%% @copyright (C) 2014, <COMPANY>
 %%% @doc
 %%%
 %%% @end
 %%% Created : 12. Nov 2014 10:52
 %%%-------------------------------------------------------------------
 -module(selections).
--author("tim_hartig").
+-author("tim_hagemann").
 
 %% API
 -export([selectionS/3]).
 selectionS(ARRAY, START, END) ->
-  SMALLEST_INDEX = findSmallest(ARRAY, START, END),
-  selectionS(switch(ARRAY, START, SMALLEST_INDEX), START + 1, END).
-
-findSmallest(ARRAY, START, END) ->
-  findSmallest(ARRAY, START, END, 0).
-
-findSmallest(_, START, END, CURRENT) when START > END ->
-  CURRENT;
-
-findSmallest(ARRAY, START, END, CURRENT) ->
-  NEXT = array:getA(ARRAY, START),
-  if NEXT > CURRENT ->
-    findSmallest(ARRAY, START + 1, END, START);
-    true ->
-      findSmallest(ARRAY, START + 1, END, CURRENT)
-  end.
-
-switch(ARRAY, FIRST, SECOND) ->
-  TEMP = array:getA(ARRAY, FIRST),
-  FIRST_SWAP = array:setA(ARRAY, FIRST, array:getA(ARRAY, SECOND)),
-  RESULT = array:setA(FIRST_SWAP, SECOND, TEMP),
-  RESULT.
+  ok.
