df01b569ae43684edcd7c6cb6c1ec9b67e444cff
Timmay24 <tim.hartig@haw-hamburg.de>
2014-11-19 10:58:24 +0100

    Fixed swapping of start and first elem of the open-list. Renamed NEXT to START_VALUE since it holds the value of START

--- aufgabe_3/src/quicksort.erl
+++ aufgabe_3/src/quicksort.erl
@@ -19,16 +19,21 @@
 %% API
 -export([quicksortRekursiv/3, quicksortRandom/3]).
 
+%% Hauptfunktion f. quicksortRekursiv
 quicksortRekursiv(ARRAY, START, END) ->
 	quicksortRekursiv(ARRAY, START, END, START).
-	
+
+
 quicksortRekursiv(ARRAY, START, END, _PIVOT) when (END - START) < 1 ->
 	SWAPS = util:countread(swap),
 	COMPARISON = util:countread(compare),
 	util:countreset(swap),
 	util:countreset(compare),
 	{ARRAY, {SWAPS, COMPARISON}};
-	
+
+%% Falls das uebergebene Array weniger als 12 Elemente hat,
+%% wird es mit SelectionSort sortiert
+%% und das sortierte Array zurueckgegeben
 quicksortRekursiv(ARRAY, START, END, _PIVOT) when (END - START) < 12 ->
 	{ TEMP, { SWAP_SEL, COMPARE_SEL }} = selections:selectionS(ARRAY, START, END),
 	SWAPS = util:countread(swap),
@@ -36,7 +41,8 @@ quicksortRekursiv(ARRAY, START, END, _PIVOT) when (END - START) < 12 ->
 	util:countreset(swap),
 	util:countreset(compare),
 	{ TEMP, { SWAPS + SWAP_SEL, COMPARISON + COMPARE_SEL }};
-	
+
+%%
 quicksortRekursiv(ARRAY, START, END, PIVOT) ->
 	{ TEMP, NEW_PIVOT } = insertPivot(ARRAY, START, END, PIVOT),
 	{ SORTED_LEFT, { LEFT_SWAP, LEFT_COMPARE }} = quicksortRekursiv(TEMP, START, NEW_PIVOT - 1),
@@ -71,44 +77,44 @@ quicksortRandom(ARRAY, START, END, PIVOT) ->
 	
 	
 
-	
+%% Hauptfunktion
+%% Sortiert den Pivot an der korrekten Stelle im Array ein
 insertPivot(ARRAY, START, END, PIVOT) ->
 	insertPivot(ARRAY, START, END, PIVOT, []).
-	
+
+%% Abbruch
 insertPivot(ARRAY, START, END, PIVOT, OPEN) when START == END ->
 	[LAST_OPEN | _] = OPEN,
-	io:write(OPEN), io:nl(),
 	TO_SWAP = LAST_OPEN - 1,
 	RESULT = swap(ARRAY, PIVOT, TO_SWAP),
 	{RESULT, TO_SWAP};
-	
+
+%% Sind Start und Pivot an der gleichen Stelle,
+%% rueckt der Start um eine Stelle nach rechts weiter
 insertPivot(ARRAY, START, END, PIVOT, OPEN) when START == PIVOT ->
 	insertPivot(ARRAY, START + 1, END, PIVOT, OPEN);
-	
+
+%%
 insertPivot(ARRAY, START, END, PIVOT, OPEN) ->
-	PIVOT_VALUE = array:getA(ARRAY, PIVOT),
-	NEXT = array:getA(ARRAY, START),
+	PIVOT_VALUE = array:getA(ARRAY, PIVOT), %% Wert des Pivotelements ermitteln
+	START_VALUE = array:getA(ARRAY, START), %% Wert des Elements an Startposition ermitteln
 	util:counting1(compare),
 	if
-		NEXT < PIVOT_VALUE ->
+		START_VALUE < PIVOT_VALUE -> %% Falls das Element bei Start kleiner ist, als das Pivotelement
 			if
-				OPEN == [] ->
-					insertPivot(ARRAY, START + 1, END, PIVOT, OPEN);
+				OPEN == [] ->  %% sind keine "Karten" mehr offen,
+					insertPivot(ARRAY, START + 1, END, PIVOT, OPEN);  %% gehe eine Position weiter
 				true ->
-					[ TO_SWAP | REST ] = OPEN,
-					TEMP = swap(ARRAY, START, TO_SWAP),
-					insertPivot(TEMP, START + 1, END, PIVOT, [START] ++ REST)
+					[ TO_SWAP | REST ] = OPEN, %% Aufteilung von Liste aufgedeckter "Karten" -> TO_SWAP: Zu tauschendes Element
+          TEMP = swap(ARRAY, START, TO_SWAP), %% Tausche
+					insertPivot(TEMP, START + 1, END, PIVOT, REST ++ [START])
 			end;
-		true ->
+		true -> %%
 			insertPivot(ARRAY, START + 1, END, PIVOT, OPEN ++ [START])
 	end.
 	
 randomPivot(START, END) ->
-	io:write(START), io:nl(), io:write(END), io:nl(),
-	RESULT = random:uniform(END - START + 1) + START - 1,
-	io:write(RESULT),
-	io:nl(),
-	RESULT.
+	random:uniform(END - START + 1) + START - 1.
 	
 %% Tauscht zwei Element in der array, zwische POS und POS2.
 swap(ARRAY, POS, POS2) ->
