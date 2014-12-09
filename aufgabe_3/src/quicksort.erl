f7e9e767a0c0611f8b2b8919929d4f504b5ead26
Timmay24 <tim.hartig@haw-hamburg.de>
2014-12-09 22:17:28 +0100

    Renamed variables. Added comments.

--- aufgabe_3/src/quicksort.erl
+++ aufgabe_3/src/quicksort.erl
@@ -19,11 +19,11 @@
 %% API
 -export([quicksortRekursiv/3, quicksortRandom/3]).
 
-%% Hauptfunktion f. quicksortRekursiv
+%% Hauptfunktion f. quicksortRekursiv mit linkestem Element als Pivot
 quicksortRekursiv(ARRAY, START, END) ->
 	quicksortRekursiv(ARRAY, START, END, START).
 
-
+%% Aufruf, falls das Ende erreicht bzw. 'ueberschritten' wurde
 quicksortRekursiv(ARRAY, START, END, _PIVOT) when (END - START) < 1 ->
 	SWAPS = util:countread(swap),
 	COMPARISON = util:countread(compare),
@@ -42,82 +42,111 @@ quicksortRekursiv(ARRAY, START, END, _PIVOT) when (END - START) < 12 ->
 	util:countreset(compare),
 	{ TEMP, { SWAPS + SWAP_SEL, COMPARISON + COMPARE_SEL }};
 
-%%
+%% Regulaerer Aufruf
 quicksortRekursiv(ARRAY, START, END, PIVOT) ->
   %% ARRAY partitionieren
-	{ARRAY_WITH_CORRECT_PIVOT, NEW_PIVOT } = insertPivot(ARRAY, START, END, PIVOT),
-  %% Linkes Teilarray von ARRAY an rekursiven Aufruf uebergeben
-	{ SORTED_LEFT, { LEFT_SWAP, LEFT_COMPARE }} = quicksortRekursiv(ARRAY_WITH_CORRECT_PIVOT, START, NEW_PIVOT - 1),
-	{ RESULT, { RIGHT_SWAP, RIGHT_COMPARE }} = quicksortRekursiv(SORTED_LEFT, NEW_PIVOT + 1, END),
-	{ RESULT, { LEFT_SWAP + RIGHT_SWAP, LEFT_COMPARE + RIGHT_COMPARE }}.
+	{PARTITIONED_ARRAY, NEW_PIVOT} = insertPivot(ARRAY, START, END, PIVOT),
+  %% Teil, links vom Pivot, des Arrays sortieren
+	{ARRAY_WITH_LEFT_PART_SORTED, {LEFT_SWAPS, LEFT_COMPARES}} = quicksortRekursiv(PARTITIONED_ARRAY, START, NEW_PIVOT - 1),
+  %% Teil, rechts vom Pivot, des Arrays mit links sortiertem Teil, sortieren
+	{SORTED_ARRAY, {RIGHT_SWAPS, RIGHT_COMPARES}} = quicksortRekursiv(ARRAY_WITH_LEFT_PART_SORTED, NEW_PIVOT + 1, END),
+
+  %% Sortiertes Array mit zusammengezaehlten Messdaten zurueckgeben
+	{SORTED_ARRAY, {LEFT_SWAPS + RIGHT_SWAPS, LEFT_COMPARES + RIGHT_COMPARES}}.
 
 
-
-	
+%% Hauptfunktion f. quicksortRandom mit zufaelligem Pivot
 quicksortRandom(ARRAY, START, END) ->
 	quicksortRandom(ARRAY, START, END, randomPivot(START, END)).
 
+%% Aufruf, falls das Ende erreicht oder 'ueberschritten' wurde:
+%% Das sortierte Array und die Messdaten werden zurueckgegeben
 quicksortRandom(ARRAY, START, END, _PIVOT) when (END - START) < 1 ->
 	SWAPS = util:countread(swap),
 	COMPARISON = util:countread(compare),
 	util:countreset(swap),
 	util:countreset(compare),
 	{ARRAY, {SWAPS, COMPARISON}};
-	
+
+%% Aufruf fuer Arrays bzw. Bereichen mit weniger als 12 Elementen
+%% => Sortierung des Teils wird mit Selection-Sort durchgefuehrt
 quicksortRandom(ARRAY, START, END, _PIVOT) when (END - START) < 12 ->
-	{TEMP, {SWAP_SEL, COMPARE_SEL}} = selections:selectionS(ARRAY, START, END),
+	{SORTED_ARRAY, {SWAPS_SEL, COMPARES_SEL}} = selections:selectionS(ARRAY, START, END),
 	SWAPS = util:countread(swap),
 	COMPARISON = util:countread(compare),
 	util:countreset(swap),
 	util:countreset(compare),
-	{TEMP, {SWAPS + SWAP_SEL, COMPARISON + COMPARE_SEL}};
-	
+	{SORTED_ARRAY, {SWAPS + SWAPS_SEL, COMPARISON + COMPARES_SEL}};
+
+%% Regulaerer Aufruf
 quicksortRandom(ARRAY, START, END, PIVOT) ->
-	{TEMP, NEW_PIVOT} = insertPivot(ARRAY, START, END, PIVOT),
-	if
+  %% ARRAY partitionieren
+	{PARTITIONED_ARRAY, NEW_PIVOT} = insertPivot(ARRAY, START, END, PIVOT),
+
+	if% Falls nach der Partitionierung das Pivot...
+    % ...an das Ende gerueckt wurde...
 		NEW_PIVOT == END ->
-			{ SORTED_LEFT, { LEFT_SWAP, LEFT_COMPARE }} = quicksortRandom(TEMP, START, NEW_PIVOT - 1),
-			{ RESULT, { RIGHT_SWAP, RIGHT_COMPARE }} = { SORTED_LEFT, { 0, 0 } };
+      % ...muss 'nur' der Teil links vom Pivot uebergeben werden (ein lokaler worst case)...
+			{ ARRAY_WITH_LEFT_PART_SORTED, {LEFT_SWAPS, LEFT_COMPARES}} = quicksortRandom(PARTITIONED_ARRAY, START, NEW_PIVOT - 1),
+			% ...und fuer den fiktiven, rechten Teil werden die Swaps und Compares auf 0 gesetzt
+      { SORTED_ARRAY, {RIGHT_SWAPS, RIGHT_COMPARES}} = {ARRAY_WITH_LEFT_PART_SORTED, { 0, 0 } };
+
+    % ...an den Anfang gerueckt wurde...
 		NEW_PIVOT == START ->
-			{ SORTED_LEFT, { LEFT_SWAP, LEFT_COMPARE }} = { TEMP, { 0, 0 } },
-			{ RESULT, { RIGHT_SWAP, RIGHT_COMPARE }} = quicksortRandom(SORTED_LEFT, NEW_PIVOT + 1, END);
+      % ...werden fuer den fiktiven, linken Teil Swaps und Compares auf 0 gesetzt und...
+			{ ARRAY_WITH_LEFT_PART_SORTED, {LEFT_SWAPS, LEFT_COMPARES}} = {PARTITIONED_ARRAY, { 0, 0 } },
+      % ...es muss 'nur' der Teil rechts vom Pivot uebergeben werden (ein lokaler worst case)
+			{ SORTED_ARRAY, {RIGHT_SWAPS, RIGHT_COMPARES}} = quicksortRandom(ARRAY_WITH_LEFT_PART_SORTED, NEW_PIVOT + 1, END);
+
+    % ...zwischen Anfang und Ende gerueckt wurde...
 		true ->
-			{ SORTED_LEFT, { LEFT_SWAP, LEFT_COMPARE }} = quicksortRandom(TEMP, START, NEW_PIVOT - 1),
-			{ RESULT, { RIGHT_SWAP, RIGHT_COMPARE }} = quicksortRandom(SORTED_LEFT, NEW_PIVOT + 1, END)
+      % ...wird erst der Teil links vom Pivot sortiert...
+			{ ARRAY_WITH_LEFT_PART_SORTED, {LEFT_SWAPS, LEFT_COMPARES}} = quicksortRandom(PARTITIONED_ARRAY, START, NEW_PIVOT - 1),
+      % ...und anschließend der rechte Teile des Arrays mit links sortiertem Teil sortiert
+			{ SORTED_ARRAY, {RIGHT_SWAPS, RIGHT_COMPARES}} = quicksortRandom(ARRAY_WITH_LEFT_PART_SORTED, NEW_PIVOT + 1, END)
 	end,
-	{ RESULT, { LEFT_SWAP + RIGHT_SWAP, LEFT_COMPARE + RIGHT_COMPARE }}.
+
+  % Sortiertes Array mit zusammengezaehlten Messdaten zurueckgeben
+	{ SORTED_ARRAY, { LEFT_SWAPS + RIGHT_SWAPS, LEFT_COMPARES + RIGHT_COMPARES}}.
 
 
-%% Hauptfunktion
-%% Sortiert den Pivot an der korrekten Stelle im Array ein
+%% Partitioniert das ARRAY, indem es das Pivot an die korrekte Stelle im ARRAY einrueckt
+%% (Hauptfunktion)
 insertPivot(ARRAY, START, END, PIVOT) ->
 	insertPivot(ARRAY, START, END, PIVOT, []).
 
 %% Abbruch
 insertPivot(ARRAY, CURPOS, END, PIVOT, OPEN_CARDS) when CURPOS > END ->
 	if
+    % Falls keine Karten offen sind...
 		OPEN_CARDS == [] ->
+      % ...wird Pivot mit Element am Ende getauscht
 			RESULT = swap(ARRAY, PIVOT, END),
 			{RESULT, END};
+
+    % Falls es offene Karten gibt...
 		true ->
+      % ...hole die erste Karte
 			[FIRST_OPEN_CARD | _] = OPEN_CARDS,
-			LAST_CLOSED_CARD = FIRST_OPEN_CARD - 1, %% Position der letzten geschlossenen Karte
+      % und die Position der letzten geschlossenen Karte
+			LAST_CLOSED_CARD = FIRST_OPEN_CARD - 1,
 
 			if
-			%% liegt das Pivot innerhalb der offenen Karten
+			%% Ist das Pivot unter den offenen Karten...
 				PIVOT > FIRST_OPEN_CARD ->
-					%% tausche es mit der ersten offenen Karte
+					%% ...tausche es mit der ersten offenen Karte
 					RESULT = swap(ARRAY, PIVOT, FIRST_OPEN_CARD),
 					NEW_POS = FIRST_OPEN_CARD;
 
-			%% liegt das Pivot vor der letzten geschlossenen Karte
+			%% Liegt das Pivot vor der letzten geschlossenen Karte...
 				PIVOT < LAST_CLOSED_CARD ->
-					%% tausche es mit der letzten geschlossenen Karte
+					%% ...tausche es mit der letzten geschlossenen Karte
 					RESULT = swap(ARRAY, PIVOT, LAST_CLOSED_CARD),
 					NEW_POS = LAST_CLOSED_CARD;
 
-			%% liegt das Pivot bereits an der richtigen Stelle
+			%% Liegt das Pivot bereits an der richtigen Stelle...
 				true ->
+          % dann ist alles prima und das Array kann so weitergegeben werden
 					RESULT = ARRAY,
 					NEW_POS = PIVOT
 			end,
@@ -129,29 +158,39 @@ insertPivot(ARRAY, CURPOS, END, PIVOT, OPEN_CARDS) when CURPOS > END ->
 insertPivot(ARRAY, CURPOS, END, PIVOT, OPEN) when CURPOS == PIVOT ->
 	insertPivot(ARRAY, CURPOS + 1, END, PIVOT, OPEN);
 
-%%
+%% Regulaerer Aufruf
 insertPivot(ARRAY, CURPOS, END, PIVOT, OPEN_CARDS) ->
-	%% OPEN: "Open-Cards-List" - enthaelt die Positionen aller Elemente, die groesser als das Pivot sind
+	%% OPEN_CARDS: "Open-Cards-List" - enthaelt die Positionen aller Elemente, die groesser als das Pivot sind
 
-  PIVOT_VALUE = array:getA(ARRAY, PIVOT), %% Wert des Pivotelements ermitteln
-	CURPOS_VALUE = array:getA(ARRAY, CURPOS), %% Wert des Elements an aktueller Position ermitteln
+  %% Wert des Pivotelements ermitteln
+  PIVOT_VALUE = array:getA(ARRAY, PIVOT),
+  %% Wert des Elements an aktueller Position ermitteln
+	CURPOS_VALUE = array:getA(ARRAY, CURPOS),
 	util:counting1(compare),
 	if
-		CURPOS_VALUE < PIVOT_VALUE -> %% Falls das Element an aktueller Position kleiner ist, als das Pivotelement
+    %% Falls das Element an aktueller Position kleiner als das Pivot ist...
+		CURPOS_VALUE < PIVOT_VALUE ->
 			if
-				OPEN_CARDS == [] ->  %% und keine "Karten" mehr offen sind,
-					insertPivot(ARRAY, CURPOS + 1, END, PIVOT, OPEN_CARDS);  %% gehe eine Position weiter
+        %% ...und keine "Karten" mehr offen sind,...
+				OPEN_CARDS == [] ->
+          %% ...gehe eine Position weiter
+					insertPivot(ARRAY, CURPOS + 1, END, PIVOT, OPEN_CARDS);
 				true ->
-					[ TO_SWAP | REST ] = OPEN_CARDS, %% teile die Liste aufgedeckter "Karten" auf -> TO_SWAP: Zu tauschendes Element
-          TEMP = swap(ARRAY, CURPOS, TO_SWAP), %% Tausche die Elemente
-					insertPivot(TEMP, CURPOS + 1, END, PIVOT, REST ++ [CURPOS]) %% fuege Element von aktueller Position
-                                                                      %% ans Ende der Open-Cards-List an
+          %% ...teile die Liste aufgedeckter "Karten" auf (TO_SWAP: Zu tauschendes Element)
+					[ TO_SWAP | REST ] = OPEN_CARDS,
+          %% Tausche die Elemente
+          TEMP = swap(ARRAY, CURPOS, TO_SWAP),
+          %% fuege Element von aktueller Position ans Ende der Open-Cards-List an
+					insertPivot(TEMP, CURPOS + 1, END, PIVOT, REST ++ [CURPOS])
 			end;
-		true -> %% wenn Element bei aktueller Position nicht kleiner ist
-			insertPivot(ARRAY, CURPOS + 1, END, PIVOT, OPEN_CARDS ++ [CURPOS]) %% fuege Element von aktueller Position
-                                                                         %% ans Ende der Open-Cards-List an
+    %% Falls das Element an aktueller Position nicht kleiner als das Pivot ist...
+		true ->
+      %% ...fuege das Element ans Ende der Open-Cards-List an...
+      %% ...und gehe einen Schritt weiter
+			insertPivot(ARRAY, CURPOS + 1, END, PIVOT, OPEN_CARDS ++ [CURPOS])
 	end.
-	
+
+%% Wahl einer zufaelligen Position innerhalb des vorgegebenen Bereichs, die als Pivot fungieren soll
 randomPivot(START, END) ->
 	if
 		(END - START) =< 1 ->
