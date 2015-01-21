a7a336c10b7ad8d5bbd5588ed93f3570c718f6e4
kumpelblase2 <tim@infinityblade.de>
2015-01-21 10:26:01 +0100

    Main application loop.

---
+++ aufgabe_4/main.erl
@@ -0,0 +1,43 @@
+-module(main).
+-author("tim_hagemann & tim_hartig").
+
+-export([readTree/1]).
+
+
+readTree(FileName) ->
+    LIST = util:zahlenfolgeRT(FileName),
+    listToTree(LIST).
+
+%% Konvertiert eine Erlang-Liste zu einem rekursiv geschachtelten Tupel
+listToTree(List) ->
+    if
+        is_list(List) ->
+            listToTree(List, avlbaum:create());
+        true ->
+            {error, not_a_list}
+    end.
+
+listToTree([H|T], Accu) ->
+    NewAccu = avlbaum:insert(Accu, H),
+    listToTree(T, NewAccu);
+
+listToTree([], Accu) ->
+    Accu.
+
+deleteRandom(AVL, FileName) ->
+    deleteRandomList(AVL, 25, util:zahlenfolgeRT(FileName)).
+
+deleteRandomList(AVL, _, [])
+    AVL;
+
+deleteRandomList(AVL, 0, _) ->
+    AVL;
+
+deleteRandomList(AVL, Remaining, [H|T]) ->
+    RANDOM = random:uniform(),
+    if
+        RANDOM >= 0.5
+            deleteRandomList(avlbaum:delete(AVL, H), Remaning - 1, T);
+        true ->
+            deleteRandomList(AVL, Remaining, T)
+    end.
